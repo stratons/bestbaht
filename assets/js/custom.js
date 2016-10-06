033e8c7492cd2671ce2881df9463da5748451e71
Mac Stratons <mac@stratonstech.com>
2016-10-06 16:54:24 +0700

    Open a new project
    Initialize a new project.

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.tab a').on('click', function (e) {
+
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    target = $(this).attr('href');
+
+    $('.tab-content > div').not(target).hide();
+
+    $(target).fadeIn(600);
+
+  });
+});
